f8c18904a73474ab7bf92dbc4d699eee4ddfe4e4
franjg <franjg88@hotmail.com>
2018-05-23 23:21:21 +0200

    nuevo commit

--- src/com/cice/ejercicio1/Main.java
+++ src/com/cice/ejercicio1/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args){
         int a = 2;
 
-        System.out.println("El resultado de la multiplicación es : " + (a*2+6/5));
+        System.out.println("El resultado de la multiplicación  es : " + (a*2+6/5));
     }
 }
